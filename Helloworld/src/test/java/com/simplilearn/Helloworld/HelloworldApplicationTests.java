4e25695f475a77999f4db05e96d09ce276175b55
mallela009 <92942717+mallela009@users.noreply.github.com>
2023-01-03 15:07:24 +0530

    Add files via upload

---
+++ Helloworld/src/test/java/com/simplilearn/Helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
